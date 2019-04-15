2e5015cf902e4bc923b02fcb92b3fda60d464543
Jesse Mazzie <jessegalt@yahoo.com>
2019-04-15 09:24:41 -0400

    Added command for joining computer to domain.

--- snippets.ps1
+++ snippets.ps1
@@ -1,2 +1,5 @@
 #Adds users to an AD group.
 Add-ADGroupMember -Identity GROUP_NAME -Members MEMBER1,MEMBER2,MEMBER3
+
+#Adds computer to domain.
+Add-Computer -DomainName DOMAIN_NAME -Credential Get-Credential
