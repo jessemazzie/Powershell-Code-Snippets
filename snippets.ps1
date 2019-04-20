966d195589553d4ee420fa0f3140629b1b0e6008
Jesse Mazzie <jessegalt@yahoo.com>
2019-04-20 13:56:13 -0400

    Added snippet for sending email.

--- snippets.ps1
+++ snippets.ps1
@@ -3,3 +3,7 @@ Add-ADGroupMember -Identity GROUP_NAME -Members MEMBER1,MEMBER2,MEMBER3
 
 #Adds computer to domain.
 Add-Computer -DomainName DOMAIN_NAME -Credential Get-Credential
+
+#Send email
+$PSEmailServer = NAME_OF_SMTP_SERVER
+Send-MailMessage -From FROM_EMAIL -To TO_EMAIL(S) -Subject SUBJECT -Body BODY
